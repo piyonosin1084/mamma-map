f687d6a48e0955b4b907551dcf489a753b00cb56
piyonosin1084 <03pppo3ppp@gmail.com>
2025-03-15 01:24:33 +0900

    不要なマイグレーションファイルを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_03_11_151429) do
+ActiveRecord::Schema[7.2].define(version: 2025_03_11_061916) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
