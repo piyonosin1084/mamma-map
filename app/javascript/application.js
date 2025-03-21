edd6ab69ecebe30a7cb880d34117b76d17924b02
piyonosin1084 <03pppo3ppp@gmail.com>
2025-03-21 22:00:24 +0900

    不要なコードコメントアウト

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,5 +3,5 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 // toggleDetails.js を読み込む
 import "./toggleDetails";
-import Rails from "@rails/ujs";
-Rails.start();
+//import Rails from "@rails/ujs";
+//Rails.start();
