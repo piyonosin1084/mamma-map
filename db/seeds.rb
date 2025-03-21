4284e844d66a7b57d9b2a05ddbc2bf2b1ba4d4cc
piyonosin1084 <03pppo3ppp@gmail.com>
2025-03-22 00:09:08 +0900

    admin修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+User.create!(
+  username: "Moe(admin)"
+  email: "03pppo3ppp@gmail.com",
+  password: "Saori150325",
+  password_confirmation: "Saori150325",
+  admin: true
+)
